c7ffc20ce0eab83571f36c62b3748a80472f7839
Iddo Kenigsberg <Iddo.Kenigsberg@post.runi.ac.il>
2025-11-06 19:58:02 +0200

    fifth try

--- CalcPi.java
+++ CalcPi.java
@@ -16,7 +16,7 @@ public class CalcPi {
             mehane+=2;
         }
         System.out.println("pi according to Java: "+ Math.PI);
-        System.out.println("pi, approximated:      "+pi*4);
+        System.out.println("pi, approximated:     "+pi*4);
     }
 
 }
